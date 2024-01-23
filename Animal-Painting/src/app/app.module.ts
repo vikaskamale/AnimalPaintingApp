4e7b03068c0f9dbaafc1a5f9e6c1dab2ffaa4bcd
vikaskamale <vikas.kamale@valuemomentum.com>
2024-01-23 18:30:04 +0530

    have added cart module and service and perfromed curd using mockoon

--- Animal-Painting/src/app/app.module.ts
+++ Animal-Painting/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { CartModule } from './cart/cart.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     ProductModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    CartModule
   ],
   providers: [],
   bootstrap: [AppComponent]

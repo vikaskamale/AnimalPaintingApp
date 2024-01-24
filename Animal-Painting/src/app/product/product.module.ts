1e6453efa4c0fea0b116a7c1ab65d5480013df37
vikaskamale <vikas.kamale@valuemomentum.com>
2024-01-24 17:31:10 +0530

    created product-list module and component and applied sorting and filter

--- Animal-Painting/src/app/product/product.module.ts
+++ Animal-Painting/src/app/product/product.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import {MatCardModule} from '@angular/material/card';
 import { FlexModule } from '@angular/flex-layout';
-
-
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     ProductListComponent
@@ -12,7 +13,10 @@ import { FlexModule } from '@angular/flex-layout';
   imports: [
     CommonModule,
     MatCardModule,
-    FlexModule
+    FlexModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class ProductModule { }
